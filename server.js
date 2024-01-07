eaf81dd0d2ed955395e419d06a421cab77c9d092
Vasil Jo <vasil.jo@gmail.com>
2024-01-07 16:39:38 -0500

    add

--- server.js
+++ server.js
@@ -13,16 +13,27 @@ console.log('inc 1CC:',keys.includes("1CC"));
 // F E D C B A 9 8 7 6 5 4 3 2 1
 // 
 
+
+var lastdata = {};
+
 can.onMessage(function(msg) {
 
   console.log(util.inspect(msg, {depth: null})); //':'+msg);
 
-  if(msg.id == 0x1CC && keys.includes('1CC')) {
-    console.log('--find:1CC'); 
+  // was before
+  if(lastdata[msg.id]) {
+
+
+
+    if(msg.id == 0x1CC && keys.includes('1CC')) {
+      console.log('--find:1CC'); 
+    }
+    else if(msg.id == 0x458 && keys.includes('458')) {
+      console.log('--find:458');
+    }
+
   }
-  else if(msg.id == 0x458 && keys.includes('458')) {
-    console.log('--find:458');
-  }
+  lastdata[msg.id] = msg.data;
 
 
   var outstr = can.decToHex(msg.id) + ": ";
